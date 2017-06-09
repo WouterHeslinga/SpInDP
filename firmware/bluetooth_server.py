217b7b30330e6def743749e765429be703638eb4
Kylian <kylian@127.0.0.1>
2017-06-09 12:21:31 +0200

    Basic working bluetooth server

--- firmware/bluetooth_server.py
+++ firmware/bluetooth_server.py
@@ -1,192 +1,37 @@
 import socket
 import json
+import threading
 
 from time import sleep
 from random import randint
 
-
 class BluetoothServer:
-    def __init__(self, name, legs, servos):
-        self.name = name
-        self.legs = legs
-        self.servos = servos
+    def __init__(self, queue, port=1):
+        self.workers = []
+        self.port = port
         self.socket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-        self.stopSign = False
 
     def run(self):
-        self.s_print("Starting... ")
-        if self.name == "debugApp":
-            self.startDebugAppServer()
-        elif self.name == "controller":
-            self.startControllerServer()
+        print("Starting server")
+        self.socket.bind(("00:1A:7D:DA:71:12", self.port))
+        self.socket.listen(5)
 
-    def stop(self):
-        print("Trying to close socket...",)
-        try:
-            self.stopSign = True
-            self.socket.shutdown(1)
-            self.socket.close()
-            self.s_print("Socket closed")
-        except Exception as ex:
-            self.s_print(ex)
+        while True:
+            client, address = self.socket.accept()
+            client.settimeout(60)
+            worker = threading.Thread(target=self.worker, args=(client, address))
+            self.workers.append(worker)
+            worker.start()
 
-    def bind_and_listen(self, MAC, PORT, reuse_addr=True):
-        from socket import SOL_SOCKET, SO_REUSEADDR, error
-
-        hostMACAddress = MAC  # The MAC address of a Bluetooth adapter on the server. The server might ha$
-        port = PORT  # 3 is an arbitrary choice. However, it must match the port used by the client.
-        backlog = 1
-
-        if reuse_addr:
+    def worker(self, client, address):
+        size = 1024
+        while True:
             try:
-                self.socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, self.socket.getsockopt(SOL_SOCKET, SO_REUSEADDR) | 1)
-            except error:
-                pass
-        self.socket.bind((hostMACAddress, port))
-        self.socket.listen(backlog)
-
-    def startDebugAppServer(self):
-        # bind a server port to listen for incoming connections
-        self.bind_and_listen('00:1A:7D:DA:71:12', 3)
-
-        while self.stopSign == False:
-            try:
-                self.s_print("Waiting for a connection...")
-                client, address = self.socket.accept()
-                self.s_print("Connection accepted")
-
-                while 1:
-                    data = ""
-                    # battery value
-                    data += str(randint(0, 100)) + ";"
-
-                    # inclination value
-                    data += str(randint(0, 100)) + ";"
-
-                    # servo values : 0 =  temperature, 1 = angle
-                    for x in range(1, 19):
-                        info = "0,0;"
-                        for servo in self.servos:
-                            if str(servo.id) == str(x):
-                                info = "%s,%s;" % (str(servo.getTemperature()), str(servo.getAngle()))
-                                break
-
-                        data += info
-
-                    client.send(data.encode())
-
-                    sleep(1)
-
-            except KeyboardInterrupt:
-                self.s_print("Interrupted by user")
-                if client is not None:
-                    client.close()
-                self.socket.close()
-            except Exception as ex:
-                self.s_print(str(str(ex)[:7]))
-                if str(str(ex)[:7]) in "Timeout":
-                    self.s_print("Exception: " + str(ex))
-                    pass
-
-                self.s_print("Closing socket with exception" + str(ex))
-                if client is not None:
-                    client.close()
-                self.socket.close()
-                break
-
-    def startControllerServer(self):
-        # bind a server port to listen for incoming connections
-        # self.bind_and_listen('00:1A:7D:DA:71:12', 1)
-        self.socket.settimeout(2)
-        self.socket.setblocking(0)
-        serverMACAddress = '98:D3:31:FD:17:0D'
-
-        while self.stopSign is False:
-            try:
-                while True:
-                    try:
-                        self.socket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-                        self.s_print("Trying to connect")
-                        self.socket.connect((serverMACAddress, 1))
-                        self.s_print(self.socket)
-                        break
-                    except Exception as ex:
-                        print(str(ex))
-                        self.s_print("Failed to connect")
-                        sleep(4)
-
-                self.s_print("Connected")
-
-                amount = 0
-                error = 0
-                data = ""
-                while True:
-                    sleep(.2)
-                    data_end = -1
-                    self.socket.send("1")
-                    try:
-                        while data_end == -1:
-
-                            try:
-                                print
-                                "trying to receive..."
-                                data += self.socket.recv(1024)
-
-                            except:
-                                if error == 1:
-                                    error_start = True
-                                    print(error)
-                                    break
-                                else:
-                                    error_start = False
-                                    break
-                                """ CAN THIS BE DELETED???
-                                if error_start == True and error >= 10 or error >= 20 or eroor >= 30 or error >= 40 or eroor >= 50:
-                                    print
-                                    "Error count" + error"""
-                                error += 1
-                                break
-
-                            data_end = data.find('\n')
-
-                            if data_end != -1:
-                                rec = data[:data_end]
-                                try:
-                                    data_json = rec
-                                    load_json = json.loads(data_json)
-                                    # Dit zijn de programma/races van de smartconroller
-                                    program1 = load_json["program1"]
-                                    program2 = load_json["program2"]
-                                    print(program1)
-                                    print(program2)
-
-                                    amount = amount + 1
-                                    data = ""
-                                except(ValueError, KeyError, TypeError):
-                                    print("JSON format error")
-                                    self.socket.close()
-                                    time.sleep(1)
-                                rec = rec[data_end + 1:]
-
-                    except KeyboardInterrupt:
-                        self.s_print("keyboardInterrupt")
-                    except socket.timeout:
-                        self.s_print("socket timeout")
-                    except socket.error:
-                        self.s_print("socket error")
-                    except Exception as ex:
-                        print("Failed to receive: " + str(ex))
-
-            except KeyboardInterrupt:
-                self.s_print("Interrupted by user")
-                self.socket.close()
-            except Exception as ex:
-                if str(ex)[:7] not in "Timeout":
-                    self.s_print("timeout")
-                self.s_print("Closing socket with exception" + str(ex))
-                self.socket.close()
-                break
-
-    def s_print(self, message):
-        print("[" + self.name + "] " + str(message))
-
+                data = client.recv(size)
+                if data:
+                    print(data)
+                else:
+                    raise "Client disconnected"
+            except:
+                client.close()
+                return False
