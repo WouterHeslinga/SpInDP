76474934a6fbf590ddecd777721ab33b42832dbb
Kylian <kylian@127.0.0.1>
2017-06-09 12:21:31 +0200

    Bluetooth refactor

--- firmware/main.py
+++ firmware/main.py
@@ -23,29 +23,27 @@ def web_worker(queue):
     return
 
 
-def motion_controller_worker():
+def motion_controller_worker(queue):
     """Worker for the leg controller"""
     print("Starting leg controller worker")
     motion = MotionController()
     motion.update()
 
-
-def bluetooth_server_worker():
+def bluetooth_server_worker(queue):
     """Worker for the bluetooth server"""
-    print("Starting bluetooth debugApp worker")
-    bluetoothapp = BluetoothServer("debugApp", [1-6], [1-18])
-    print("Starting bluetooth controller worker")
-    bluetoothcontroller = BluetoothServer("controller", [1-6], [1-18])
+    print("Starting bluetooth worker")
+    bluetooth = BluetoothServer(queue, 1)
 
 if __name__ == '__main__':
     workers = []
     queue_vision_web = multiprocessing.Queue()
+    queue_bluetooth_motion = multiprocessing.Queue()
 
     # Create the workers
     workers.append(multiprocessing.Process(target=vision_worker, args=(queue_vision_web,)))
     workers.append(multiprocessing.Process(target=web_worker, args=(queue_vision_web,)))
-    workers.append(multiprocessing.Process(target=motion_controller_worker))
-    workers.append(multiprocessing.Process(target=bluetooth_server_worker))
+    workers.append(multiprocessing.Process(target=motion_controller_worker, args=(queue_bluetooth_motion,)))
+    workers.append(multiprocessing.Process(target=bluetooth_server_worker, args=(queue_bluetooth_motion,)))
 
     # Start the workers
     for worker in workers:
--- firmware/motion_controller.py
+++ firmware/motion_controller.py
@@ -2,8 +2,9 @@ from time import sleep
 
 
 class MotionController:
-    def __init__(self):
+    def __init__(self, queue):
         self.test = "Test"
+        self.queue = queue
 
     def update(self):
         while True:
