e6084cb05b977fd24535d02888cc85509e94a990
Kylian <kylian@127.0.0.1>
2017-06-13 18:04:17 +0200

    Some documentation for the bluetooth server

--- firmware/bluetooth_server.py
+++ firmware/bluetooth_server.py
@@ -13,9 +13,10 @@ class BluetoothServer:
         self.socket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 
     def run(self):
+        """The method that starts the server and accepts connections"""
         print("Starting server")
         self.socket.bind(("00:1A:7D:DA:71:12", self.port))
-        self.socket.listen(5)
+        self.socket.listen(5) # Max 5 clients, not port
 
         while True:
             client, address = self.socket.accept()
@@ -25,12 +26,15 @@ class BluetoothServer:
             worker.start()
 
     def worker(self, client, address):
+        """The worker method that handles the connection form the client, each one runs in a seperate thread"""
         size = 1024
         print("Client %s connected" % address[0])
 
         data = ""
         while True:
             try:
+                # Because most clients streams the data rather than send it all at once, the commands needs to be split at a line ending 
+                # Multiple commands can be send at once, therefor this loop
                 data = data + client.recv(size)
                 while True:
                     end = data.find('\n')
@@ -41,7 +45,7 @@ class BluetoothServer:
                     split = command.split(":")
                     self.queue.put(split)
                     print("Server recieved: %s" % ", ".join(split))
-                    data = data[end+1:]
+                    data = data[end+1:] # A half command can exist after send(1024) from the client, so we still need to keep this information
 
             except Exception as ex:
                 client.close()
