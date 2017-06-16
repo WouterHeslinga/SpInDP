0d977d79efffc296009dbedb9ddfbd4187a17c42
Wouter <Wouter_heslinga@hotmail.com>
2017-06-16 10:30:28 +0200

    Test balloon 7

--- firmware/vision.py
+++ firmware/vision.py
@@ -140,6 +140,7 @@ class Vision:
     def get_round_contour(self, hsv, frame):
         mask = self.color_filter(hsv, 'redballoon')
         _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
         for i in range(len(contours)):
             contour = contours[i]
             area = cv2.contourArea(contour)
@@ -149,11 +150,6 @@ class Vision:
             factor = 4 * math.pi * area / perimeter ** 2
             if factor > .5:
                 cv2.drawContours(frame, [contour], -1, (0, 255, 0), 2)
-                M = cv2.moments(contour)
-                cx = int(M['m10'] / M['m00'])
-                cy = int(M['m01'] / M['m00'])
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(frame, ("Factor: %f" % factor), (cx, cy), font, .5, (0, 255, 0), 2)
             self.show_image('round shape', frame)
 
     def best_matching_shape(self, contours, shape):
