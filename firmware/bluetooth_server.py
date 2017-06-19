5f7b4d8538aef2aa474026dcd68e8e24e43588ad
RaXe-Robert <robertbijl@live.nl>
2017-06-19 15:15:49 +0000

    Some bluetooth server changes

--- firmware/bluetooth_server.py
+++ firmware/bluetooth_server.py
@@ -63,6 +63,7 @@ class BluetoothServer:
                     
                     command = data[:end]
                     split = command.split(":")
+                    print(split)
                     self.execute(client, split)
                     data = data[end+1:] # A half command can exist after send(1024) from the client, so we still need to keep this information
 
